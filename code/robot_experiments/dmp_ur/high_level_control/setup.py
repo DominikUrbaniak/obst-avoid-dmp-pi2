eb4d04f39a7a3f9ff4e5e8aaf4f1f92f5caf71de
DominikUrbaniak <126696875+DominikUrbaniak@users.noreply.github.com>
2025-11-14 19:54:39 +0100

    Add files via upload

--- code/robot_experiments/dmp_ur/high_level_control/setup.py
+++ code/robot_experiments/dmp_ur/high_level_control/setup.py
@@ -1,4 +1,5 @@
 from setuptools import find_packages, setup
+from glob import glob
 
 package_name = 'high_level_control'
 
@@ -10,6 +11,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/docs/dmp/', glob('docs/dmp/**/*', recursive=True)),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
